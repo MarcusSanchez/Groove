0760194193cf498555a370db1a01f5fd143bde98
Marcus Sanchez <marcussanchez031@gmail.com>
2024-01-03 18:59:57 -0500

    refactor User Validation

--- backend/db/validators.go
+++ backend/db/validators.go
@@ -4,21 +4,60 @@ import (
 	"regexp"
 )
 
+type UserField int
+
+const (
+	FieldUsername UserField = iota + 1
+	FieldEmail
+	FieldPassword
+)
+
+type validator struct {
+	Field   UserField
+	Regex   *regexp.Regexp
+	Message string
+}
+
+var userValidators = []validator{
+	{
+		Field:   FieldPassword,
+		Regex:   regexp.MustCompile(`^.{8,32}$`),
+		Message: "invalid password: must be between 8 and 32 characters",
+	}, {
+		Field:   FieldPassword,
+		Regex:   regexp.MustCompile(`^[a-zA-Z].*\d|.*[a-zA-Z].*\d|.*[a-zA-Z]\d.*$`),
+		Message: "invalid password: must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number",
+	}, {
+		Field:   FieldUsername,
+		Regex:   regexp.MustCompile(`^.{4,16}$`),
+		Message: "invalid username: must be between 4 and 16 characters",
+	}, {
+		Field:   FieldEmail,
+		Regex:   regexp.MustCompile(`^.{4,320}$`),
+		Message: "invalid email: must be between 4 and 320 characters",
+	}, {
+		Field:   FieldEmail,
+		Regex:   regexp.MustCompile(`^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$`),
+		Message: "invalid email: must be a valid email address",
+	},
+}
+
 func ValidateUser(username, email, password string) string {
-	if !regexp.MustCompile(`^.{8,32}$`).MatchString(password) {
-		return "invalid password: must be between 8 and 32 characters"
-	}
-	if !regexp.MustCompile(`^[a-zA-Z].*\d|.*[a-zA-Z].*\d|.*[a-zA-Z]\d.*$`).MatchString(password) {
-		return "invalid password: must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number"
-	}
-	if !regexp.MustCompile(`^.{4,16}$`).MatchString(username) {
-		return "invalid username: must be between 4 and 16 characters"
-	}
-	if !regexp.MustCompile(`^.{4,320}$`).MatchString(email) {
-		return "invalid email: must be between 4 and 320 characters"
-	}
-	if !regexp.MustCompile(`^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$`).MatchString(email) {
-		return "invalid email: must be a valid email address"
+	for _, v := range userValidators {
+		var field string
+
+		switch v.Field {
+		case FieldUsername:
+			field = username
+		case FieldEmail:
+			field = email
+		case FieldPassword:
+			field = password
+		}
+
+		if !v.Regex.MatchString(field) {
+			return v.Message
+		}
 	}
 	return ""
 }
